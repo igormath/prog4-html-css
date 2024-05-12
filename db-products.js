7ae1286d9cc7eff6e90c1c2e9231ac1bbc6b44d9
igormath <igorxt9@gmail.com>
2024-05-12 13:26:29 -0300

    Update db-products price type

--- db-products.js
+++ db-products.js
@@ -2,51 +2,51 @@ export const products = [
     {
         "id": 1,
         "name": "Ração Úmida Pedigree Sachê Cães Adultos Raças Pequenas – Carne ao Molho 100g",
-        "price": "3,20",
+        "price": 3.20,
     },
     {
         "id": 2,
         "name": "Orelha Bovina Para Cães Bonapet 1 Unidade",
-        "price": "5,18",
+        "price": 5.18,
     },
     {
         "id": 3,
         "name": "Osso Rótula Bovina Para Cães Bonapet",
-        "price": "5,84",
+        "price": 5.84,
     },
     {
         "id": 4,
         "name": "Ração Úmida Optimum Sachê Gatos Castrados Frango 85g",
-        "price": "3,20",
+        "price": 3.20,
     },
     {
         "id": 5,
         "name": "Petisco Whiskas Temptations Anti Bola de Pelo Gatos Adultos – 40g",
-        "price": "3,20",
+        "price": 3.20,
     },
     {
         "id": 6,
         "name": "Ração Úmida Sache Magnus Gato Castrado Sabor Frango 85gr",
-        "price": "2,76",
+        "price": 2.76,
     },
     {
         "id": 7,
         "name": "Royal Canin Canine Veterinary Diabetic Wet Cães Adulto 410g",
-        "price": "41,86",
+        "price": 41.86,
     },
     {
         "id": 8,
         "name": "Antipulgas Advocate Para Gatos de 4 a 8kg 0,8ml Combo – 3 Pipetas",
-        "price": "180,31",
+        "price": 180.31,
     },
     {
         "id": 9,
         "name": "Nutripássaros Mistura P/ Calopsita Mel 500gr",
-        "price": "6,89",
+        "price": 6.89,
     },
     {
         "id": 10,
         "name": "Ração Amigos Do Louro José Megazoo Para Calopsita Mix 500g",
-        "price": "35,91",
+        "price": 35.91,
     },
 ]
